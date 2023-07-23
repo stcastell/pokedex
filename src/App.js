2211ab758966bc660dd45467801a268ddc148788
stcastell <santicastell98@hotmail.com>
2023-07-23 11:25:47 -0400

    Testing some stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
 
-const App = () => { 
+const App = () => {
 
-  // fetch('https://pokeapi.co/api/v2/pokemon/Ditto').then(res => res.json()).then(data => console.log(data)).catch(err => console.error(err));
-  
+  const [data, setData] = useState(null);
+  const [reset, setReset] = useState(false);
+
+  const clickHandler = () => {
+    reset ? setReset(false) : setReset(true);
+    console.log(reset)
+  }
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      //'https://jsonplaceholder.typicode.com/users'
+      //https://pokeapi.co/api/v2/pokemon/ditto
+      .then(res => res.json())
+      .then(data => { setData(data);  console.log(data.name)});
+  }, {});
+
   return (
     <Fragment>
-      HELLO
+      {data.name}
+      <button onClick={clickHandler}>PRESS ME WILL YA</button>
     </Fragment>
   );
 }
